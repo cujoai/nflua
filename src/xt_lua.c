df400073b737022998ecf7ce674bc037c1993666
Pedro Tammela <pctammela@cujo.com>
2019-01-09 20:06:13 +0000

    NFLUA-66 update copyright

--- src/xt_lua.c
+++ src/xt_lua.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2018 CUJO LLC
+ * Copyright (C) 2017-2019 CUJO LLC
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
